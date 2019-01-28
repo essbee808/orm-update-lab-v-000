ec5803590931db5e6e17c40f977304207905ca1c
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-28 21:28:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
   end
   
   def self.new_from_db(row)
-    binding.pry
+    #binding.pry
     new_student = self.new(name = row[1], grade = row[2])
     new_student.name = row[1]
     new_student.grade = row[2]
