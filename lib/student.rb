53bd6282d8c9db05a7e2b64ec19d1ed64d27eaf8
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-28 21:37:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,8 +63,9 @@ class Student
   def self.new_from_db(row)
     binding.pry
     new_student = self.new(name = row[1], grade = row[2])
+    new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
-    new_student.id = row[0]
+    
   end
 end
