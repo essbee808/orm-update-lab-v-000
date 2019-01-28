b6a0f49fb7456b2b1d88ad06cf044d5ecf7071b7
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-28 21:16:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,55 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      );
     SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    if self.id
+      update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+      
+     DB[:conn].execute(sql, self.name, self.grade)
+     #grab the ID of the row just inserted into the database and assign it to the value of @id attribute of this instance
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+  
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+  
+  def self.create(name, grade)
+    student = Student.new(name, grade) #create instance with two attributes
+    student.save #save instance into students table
+    student
+  end
+  
+  def self.new_from_db
+    binding.pry
+    new_student = Student.new(row)
   end
 end
